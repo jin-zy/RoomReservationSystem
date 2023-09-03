03361c3a5914d05593b41b56c91fafcf9e702534
jin-zy <1316688745@qq.com>
2023-09-04 02:08:21 +0800

    test ssh

--- files.h
+++ files.h
@@ -1,4 +1,7 @@
 #pragma once
 
-#define ADMIN_FILE	"admin.txt"
-#define 
+#define ADMIN_FILE		"admin.txt"		// 管理员文件
+#define STUDENT_FILE	"student.txt"	// 学生文件
+#define TEACHER_FILE	"teacher.txt"	// 教师文件
+#define ROOMS_FILE		"rooms.txt"		// 机房信息文件
+#define ORDER_FILE		"order.txt"		// 预约信息文件
